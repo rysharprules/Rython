b0021998ffa1a6435dc9c16245c622f3ded8ca2e
Ryan Sharp <rysharprules@gmail.com>
2020-06-24 15:56:06 +0100

    Fizzbuzz 2

--- rython/exercises/fizzbuzz/fizzbuzz_1.py
+++ rython/exercises/fizzbuzz/fizzbuzz_1.py
@@ -1,7 +1,7 @@
 def fizzbuzz():
     for index in range(1, 101):
         if index % 15 == 0:
-            print ('FizzBuzz')
+            print('FizzBuzz')
         elif index % 3 == 0:
             print('Fizz')
         elif index % 5 == 0:
---
+++ rython/exercises/fizzbuzz/fizzbuzz_2.py
@@ -0,0 +1,48 @@
+def fizzbuzz():
+    fizz = 'Fizz'
+    buzz = 'Buzz'
+    bong = 'Bong'
+    bang = 'Bang'
+
+    for index in range(1, 256):
+        fezz = ''
+        reverse = False
+        if index % 13 == 0:
+            fezz = 'Fezz' 
+        if index % 17 == 0:
+            reverse = True
+        if index % 15 == 0:
+            fifteen = fizz + fezz + buzz
+            if reverse:
+                fifteen = buzz + fezz + fizz
+            print(fifteen)
+        #elif index % 7 == 0 and index % 3 == 0:
+        elif index % 21 == 0:
+            twentyone = fizz + fezz + bang
+            if reverse:
+                twentyone = fizz + fezz + buzz
+            print(twentyone)
+        elif index % 11 == 0:
+            eleven = fezz + bong
+            if reverse:
+                eleven = bong + fezz
+            print(eleven)
+        elif index % 3 == 0:
+            print(fizz)
+        elif index % 5 == 0:
+            five = fezz + buzz
+            if reverse:
+                five = buzz + fezz
+            print(five)
+        elif index % 7 == 0:
+            seven = fezz + bang
+            if reverse:
+                seven = bang + fezz
+            print(seven)
+        else:
+            if len(fezz) > 0:
+                print(fezz)
+                continue
+            print(index)
+
+fizzbuzz()

7c6df2a272304eebdf719ddeeef7c33ff832eff4
Ryan Sharp <rysharprules@gmail.com>
2020-07-04 19:53:42 +0100

    tiny refactor

--- rython/exercises/bulk_file_renaming/app_stretch.py
+++ rython/exercises/bulk_file_renaming/app_stretch.py
@@ -6,13 +6,13 @@ def rename(path, type, new_name):
     shutil.move(path, new_name)
 
 folder = input('Choose a folder to rename the contents (files and folders) of (e.g. files3)... ')
-now = datetime.now()
+now = datetime.now().strftime("_%d-%m-%Y_%H-%M")
 
 for root, dirs, files in os.walk(folder):
     for name in files:
         path = os.path.join(root, name)
-        rename(path, 'file', path[:-4] + now.strftime("_%d-%m-%Y_%H-%M") + path[-4:])
+        rename(path, 'file', path[:-4] + now + path[-4:])
 for root, dirs, files in os.walk(folder): # new loop required otherwise sub-dir is renamed before sub-dir files which invalidates the orignal path
     for dir in dirs:
         path = os.path.join(root, dir)
-        rename(path, 'directory', path + now.strftime("_%d-%m-%Y_%H-%M"))
+        rename(path, 'directory', path + now)

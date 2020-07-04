a2ffc07ef9838221b38568400ac3248068ea89f9
Ryan Sharp <rysharprules@gmail.com>
2020-07-04 17:10:17 +0100

    bulk file renaming

---
+++ rython/exercises/bulk_file_renaming/app.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import shutil, os
+
+folder = input('Choose a folder to rename the contents of (e.g. files1/files2)... ')
+now = datetime.now()
+
+for root, dirs, files in os.walk(folder):
+    for name in files:
+        path = folder + '/' + name
+        new_name = path[:-4] + now.strftime("_%d-%m-%Y_%H-%M") + path[-4:]
+        print('renaming ' + path + ' to ' + new_name)
+        shutil.move(path, new_name)
